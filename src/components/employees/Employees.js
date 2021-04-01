024eca0cd07365abc96a7ed51b8481de69578700
HPreast <preasths1@gmail.com>
2021-04-01 15:25:11 -0500

    Finished practice React functions

---
+++ src/components/employees/Employees.js
@@ -0,0 +1,9 @@
+import React from "react"
+import "./Employees.css"
+
+export const EmployeeCard = () => (
+    <section className="employee">
+        <h2 className="employee__name">Emma Beaton</h2>
+        <div className="employee__location">Nashville Kennels North</div>
+    </section>
+)
